d974361fe2e23620de0ee211e245015aef111dc6
Gerry Guinardi <73393827+rousin1408@users.noreply.github.com>
2022-12-12 15:55:14 +0700

    make migration

---
+++ database/migrations/2022_12_12_083050_user.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class User extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user', function (Blueprint $table) {
+            $table->id();
+            $table->string('email');
+            $table->string('password');
+            $table->string('name');
+            $table->timestamp('birtdate');
+            $table->string('address');
+            $table->string('education');
+            $table->string('certification');
+            $table->string('voluntary_experience');
+            $table->string('work_experience');
+            $table->timestamps();
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user');
+    }
+}
